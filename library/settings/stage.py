8a3508d2056db3a2ebf0ccfc29356bbf17278886
Arda Soylu <arda.soylu@empa.com>
2020-02-21 02:23:33 +0300

    wsgi changes added

--- library/settings/stage.py
+++ library/settings/stage.py
@@ -28,18 +28,18 @@ DATABASES = {
 
 }
 
-"""
+
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.postgresql_psycopg2',
-        'NAME': POSTGRES_NAME,
-        'USER': POSTGRES_USER,
-        'PASSWORD': POSTGRES_PASSWORD,
-        'HOST': POSTGRES_HOST,
+        'NAME': 'library_db',
+        'USER': 'library_user',
+        'PASSWORD': 'library_pass',
+        'HOST': 'localhost',
         'PORT': '5432'
     }
 }
-"""
+
 
 def show_toolbar(request):
     return False
