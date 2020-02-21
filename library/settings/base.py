f934d1d757d37029be1c43434afb9b540379af9a
Arda Soylu <arda.soylu@empa.com>
2020-02-21 05:17:41 +0300

    last changes added base.py

--- library/settings/base.py
+++ library/settings/base.py
@@ -128,13 +128,13 @@ USE_TZ = True
 
 STATIC_URL = '/static/'
 
-#STATIC_ROOT = os.path.join(BASE_DIR,'static')
-
+STATIC_ROOT = os.path.join(BASE_DIR,'static')
+"""
 STATICFILES_DIRS = (
   os.path.join(BASE_DIR, 'static/'),
 )
-
-#STATICFILES_STORAGE = 'whitenoise.django.GzipManifestStaticFilesStorage'
+"""
+STATICFILES_STORAGE = 'whitenoise.django.GzipManifestStaticFilesStorage'
 
 
 
